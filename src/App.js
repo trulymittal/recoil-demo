701e91d74877585066b4b63ed5db0212e8523e6e
Truly Mittal <trulymittal@gmail.com>
2021-02-20 23:48:33 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  atom,
+  useRecoilState,
+  useResetRecoilState,
+  selector,
+  useRecoilValue,
+} from 'recoil';
+
+const countAtom = atom({
+  key: 'count-atom',
+  default: 15,
+});
+
+const fontSizeSelector = selector({
+  key: 'font-size-selector',
+  get: ({ get }) => {
+    const count = get(countAtom);
+    const fontSize = count * 4;
+    return fontSize;
+  },
+});
 
 function App() {
+  const [count, setCount] = useRecoilState(countAtom);
+  const resetCount = useResetRecoilState(countAtom);
+  const fontSize = useRecoilValue(fontSizeSelector);
+
+  const countValue = useRecoilValue(countAtom);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Recoil tutorial</h1>
+      <h2>The count value is : {count} </h2>
+      <h3>The font size of potty is : {fontSize} px </h3>
+      <p style={{ fontSize: fontSize }}>💩</p>
+      <button onClick={() => setCount(count + 1)}>Increase count</button>
+      <button onClick={resetCount}>reset count</button>
+      <h1>This is only count value: {countValue}</h1>
     </div>
   );
 }
